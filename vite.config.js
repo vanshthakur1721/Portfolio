8a35027c37115d62c0b3fb5a8c21b05103a50e2c
vansh <thakurvansh872@gmail.com>
2025-01-04 14:54:33 +0530

    fifth commit

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,10 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: "/",
+  build: {
+    outDir: "dist", // Ensure the output directory matches your Vercel settings
+  },
+  server: {
+    historyApiFallback: true, // Handles SPA routing during development
+  },
 })
